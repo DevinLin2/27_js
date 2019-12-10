8f0eadba834fe783ebb9c395616b115be6f34966
jchen9182 <jchen9182@gmail.com>
2019-12-10 18:46:09 -0500

    gcd works

--- funky_town.js
+++ funky_town.js
@@ -1,16 +1,22 @@
-var fact = function(n) {
-  if (n == 1) {
-    return 1;
-  }
-  return (n * (fact(n - 1)));
+var fact = function (n) {
+    if (n == 1) {
+        return 1;
+    }
+    return (n * (fact(n - 1)));
 };
 
-var fibonacci = function(n) {
-  if (n == 0) {
-    return 0;
-  }
-  if (n == 1) {
-    return 1;
-  }
-  return (fibonacci(n - 1) + fibonacci(n - 2));
+var fibonacci = function (n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return (fibonacci(n - 1) + fibonacci(n - 2));
+};
+
+var gcd = function (a, b) {
+    if (a % b  == 0) return b;
+    if (a < b) gcd(b, a);
+    return gcd(b, a % b);
 };

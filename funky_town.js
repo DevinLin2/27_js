54a328b26d3ddc0a3ba2ee9b8577be5689febeea
VitaminOh <dlin01@stuy.edu>
2019-12-10 09:30:55 -0500

    fib

--- funky_town.js
+++ funky_town.js
@@ -4,3 +4,13 @@ var fact = function(n) {
   }
   return (n * (fact(n - 1)));
 };
+
+var fibonacci = function(n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return (fibonacci(n - 1) + fibonacci(n - 2));
+};
